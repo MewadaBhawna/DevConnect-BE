2664c002e21ef9f02c9197d2973901000740e6ce
MewadaBhawna <bhawnamewada08@gmail.com>
2024-10-06 13:14:58 +0200

    exploring get, delete, post http request

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ Firstname: "Bhawna", Lastname: "Rajput" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data save successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("User Data deleted successfully");
+});
+app.use("/getServer", (req, res) => {
+  res.send("my new nodejs sever");
+});
+
 app.listen(1234, () => {
   console.log("successfully connect to server");
 });
