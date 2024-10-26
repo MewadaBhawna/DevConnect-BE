db12759ee04eb592b9e05e63333584d875b7c732
MewadaBhawna <bhawnamewada08@gmail.com>
2024-10-26 11:14:25 +0200

    first signup API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { adminAuth } = require("./middleware/adminAuth");
 const connectDatabase = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Bhawna",
+    lastName: "Mewada",
+    emailId: "Bhawna@gmail.com",
+    password: "Bhawna@123",
+    gender: "Female",
+  });
+  try {
+    await user.save();
+    res.send("User created successfully!");
+  } catch (err) {
+    res.status(400).send("User creation failed!");
+  }
+});
 connectDatabase()
   .then(() => {
     console.log("Database connection established");
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   await mongoose.connect(
-    "mongodb+srv://bhawnaislive:mMXKNRvc6pxfMbu2@namastecluster.in828.mongodb.net/"
+    "mongodb+srv://bhawnaislive:mMXKNRvc6pxfMbu2@namastecluster.in828.mongodb.net/devConnect"
   );
 };
 
